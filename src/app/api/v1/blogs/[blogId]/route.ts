4aee591fe8fcbe8ade57e381f65555cf25c0641b
weslife911 <mfonfubikoma@gmail.com>
2025-10-22 19:47:56 +0100

    made a change

--- src/app/api/v1/blogs/[blogId]/route.ts
+++ src/app/api/v1/blogs/[blogId]/route.ts
@@ -33,6 +33,7 @@ export async function GET(
     return NextResponse.json({
       success: true,
       message: "Blog fetched successfully!",
+      blog
     });
   } catch (e: unknown) {
     if (e instanceof Error) {
