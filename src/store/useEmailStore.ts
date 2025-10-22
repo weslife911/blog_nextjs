3704a870632867fb446698b62e0f7315f48f6e21
weslife911 <mfonfubikoma@gmail.com>
2025-10-22 19:20:59 +0100

    made a change

--- src/store/useEmailStore.ts
+++ src/store/useEmailStore.ts
@@ -2,7 +2,7 @@ import { axiosInstance } from "@/lib/axios";
 import { sendEmailType, useEmailStoreType } from "@/types/email";
 import { create } from "zustand"
 
-export const useEmailStore = create<useEmailStoreType>((set) => ({
+export const useEmailStore = create<useEmailStoreType>(() => ({
     sendEmail: (data: sendEmailType) => {
         return (axiosInstance.post("/contact", data));
     }
