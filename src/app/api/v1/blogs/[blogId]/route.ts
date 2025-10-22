a59e0a63af8bb541b423097388912b4f88eda39a
weslife911 <mfonfubikoma@gmail.com>
2025-10-22 19:36:49 +0100

    made a change

--- src/app/api/v1/blogs/[blogId]/route.ts
+++ src/app/api/v1/blogs/[blogId]/route.ts
@@ -1,59 +1,52 @@
-import { NextRequest, NextResponse } from "next/server"
-import Blog from "@/models/Blog"
+import { NextResponse } from "next/server";
+import Blog from "@/models/Blog";
 import { connectToDb } from "@/lib/connectToDb";
 
-interface Context {
-    params: {
-        blogId: string;
-    };
-}
-
 export async function GET(
-  request: NextRequest,
-  context: Context
-): Promise<NextResponse<{ success: boolean; message: string; }> | NextResponse<{ success: boolean; message: string; }>> {
+  { params }: { params: { blogId: string } }
+) {
+  await connectToDb();
 
-    await connectToDb();
+  try {
+    const { blogId } = params;
 
-    try {
-
-        const { blogId } = context.params;
-
-        if (!blogId) {
-            return NextResponse.json({
-                success: false,
-                message: "Blog ID is missing."
-            }, { status: 400 });
-        }
-
-        const blog = await Blog.findById(blogId).populate({
-            path: 'blogAuthorID',
-            select: 'full_name profile_pic email'
-        });
-
-        if (!blog) {
-            return NextResponse.json({
-                success: false,
-                message: "Blog not found."
-            }, { status: 404 });
-        }
-
-        return NextResponse.json({
-            success: true,
-            message: "Blog created successfully!"
-        });
-
-    } catch (e: unknown) {
-        if (e instanceof Error) {
-            return NextResponse.json({
-            success: false,
-            message: e.message
-            }, { status: 500 });
-        } else {
-            return NextResponse.json({
-            success: false,
-            message: "An error occurred while fetching the blog."
-            }, { status: 500 });
-        }
+    if (!blogId) {
+      return NextResponse.json(
+        { success: false, message: "Blog ID is missing." },
+        { status: 400 }
+      );
     }
+
+    const blog = await Blog.findById(blogId).populate({
+      path: "blogAuthorID",
+      select: "full_name profile_pic email",
+    });
+
+    if (!blog) {
+      return NextResponse.json(
+        { success: false, message: "Blog not found." },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json({
+      success: true,
+      message: "Blog fetched successfully!",
+    });
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      return NextResponse.json(
+        { success: false, message: e.message },
+        { status: 500 }
+      );
+    } else {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "An error occurred while fetching the blog.",
+        },
+        { status: 500 }
+      );
+    }
+  }
 }
