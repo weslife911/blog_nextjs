f9a82f08d61b7831eec14be2a3bc8c985a3c25c0
weslife911 <mfonfubikoma@gmail.com>
2025-10-22 20:30:30 +0100

    added cors settings in next config file

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,19 @@ const nextConfig: NextConfig = {
                 hostname: "*"
             }
         ]
+    },
+    async headers() {
+        return [
+            {
+                source: "/api/v1/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "https://nextwrite-ten.vercel.app" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" },
+                ]
+            }
+        ]
     }
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import AboutSectionOne from "@/components/About/AboutSectionOne";
 import AboutSectionTwo from "@/components/About/AboutSectionTwo";
 import Brands from "@/components/Brands";
 import ScrollUp from "@/components/Common/ScrollUp";
-import Contact from "@/components/Contact";
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import Pricing from "@/components/Pricing";
@@ -26,7 +25,6 @@ export default function Home() {
       <AboutSectionTwo />
       <Testimonials />
       <Pricing />
-      <Contact />
     </>
   );
 }
